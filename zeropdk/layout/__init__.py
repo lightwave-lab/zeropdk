bbbcf7ce677a0eeecf6aef6e781dbebba95147ad
Thomas Ferreira de Lima <tlima@princeton.edu>
2019-06-19 02:23:33 -0400

    adding angle option to layout_pgtext

--- zeropdk/layout/__init__.py
+++ zeropdk/layout/__init__.py
@@ -6,13 +6,13 @@ def insert_shape(cell, layer, shape):
 import klayout.db as kdb
 
 
-def layout_pgtext(cell, layer, x, y, text, mag, inv=False):
+def layout_pgtext(cell, layer, x, y, text, mag, inv=False, angle=0):
     layout = kdb.Layout()
     lylayer = layout.layer(layer)
     pcell = layout.create_cell(
         "TEXT", "Basic", {"text": text, "layer": layer, "mag": mag, "inverse": inv}
     )
-    pcell.transform_into(kdb.DTrans(kdb.DTrans.R0, x, y))
+    pcell.transform_into(kdb.DCplxTrans(1, angle, False, x, y))
     lylayer_new = cell.layout().layer(layer)
     cell.shapes(lylayer_new).insert(pcell.shapes(lylayer))
 
