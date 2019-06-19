2a4c94c1b36970351c6122897348afea5e8b89b0
Thomas Ferreira de Lima <tlima@princeton.edu>
2019-06-19 18:28:16 -0400

    Multiline layout_pgtext support

--- zeropdk/layout/__init__.py
+++ zeropdk/layout/__init__.py
@@ -9,13 +9,13 @@ import klayout.db as kdb
 def layout_pgtext(cell, layer, x, y, text, mag, inv=False, angle=0):
     layout = kdb.Layout()
     lylayer = layout.layer(layer)
-    pcell = layout.create_cell(
-        "TEXT", "Basic", {"text": text, "layer": layer, "mag": mag, "inverse": inv}
-    )
-    pcell.transform_into(kdb.DCplxTrans(1, angle, False, x, y))
-    lylayer_new = cell.layout().layer(layer)
-    cell.shapes(lylayer_new).insert(pcell.shapes(lylayer))
-
+    for i, line in enumerate(text.splitlines()):
+        pcell = layout.create_cell(
+            "TEXT", "Basic", {"text": line, "layer": layer, "mag": mag, "inverse": inv}
+        )
+        pcell.transform_into(kdb.DCplxTrans(1, angle, False, x, y - i * mag * 5 / 4))
+        lylayer_new = cell.layout().layer(layer)
+        cell.shapes(lylayer_new).insert(pcell.shapes(lylayer))
 
 from .polygons import *
 from .waveguides import *
