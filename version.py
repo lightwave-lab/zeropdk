884ea48618ad9050047d00a2040b528b337eaf22
Thomas Ferreira de Lima <tlima@princeton.edu>
2019-11-15 10:51:37 -0500

    v19.11

--- version.py
+++ version.py
@@ -1,9 +1,9 @@
 # version.py
 
 # The short X.Y version.
-version = "19.10"
+version = "19.11"
 # The full version, including alpha/beta/rc tags.
-release = version + "b1"
+release = version # + "b0"
 
 if __name__ == "__main__":
     print("zeropdk v" + version)
