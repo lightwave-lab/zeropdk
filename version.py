feed134fc3243655f93cfd5b3bd5b65ea928bf48
Thomas Ferreira de Lima <tlima@princeton.edu>
2020-04-24 17:31:39 -0400

    v20.04

--- version.py
+++ version.py
@@ -1,9 +1,9 @@
 # version.py
 
 # The short X.Y version.
-version = "19.11"
+version = "20.04"
 # The full version, including alpha/beta/rc tags.
-release = version # + "b0"
+release = version + "b0"
 
 if __name__ == "__main__":
     print("zeropdk v" + version)
